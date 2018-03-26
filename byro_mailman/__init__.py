07654b3c6ffb642cee88aa91aa0d52903027b7bc
Tobias Kunze <rixx@cutebit.de>
2018-03-26 17:33:15 +0200

    Initial commit

---
+++ byro_mailman/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'byro_mailman'
+    verbose_name = 'The byro Mailman plugin'
+
+    class ByroPluginMeta:
+        name = ugettext_lazy('The byro Mailman plugin')
+        author = 'rixx'
+        description = ugettext_lazy('Mailing list integration for byro')
+        visible = True
+        version = '0.0.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'byro_mailman.PluginApp'
