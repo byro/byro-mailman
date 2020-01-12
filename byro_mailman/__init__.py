7632d81a670450ed27a22e9b68a53bdb027e6810
Tobias Kunze <r@rixx.de>
2020-01-12 02:20:16 +0100

    Release v1.0.1

--- byro_mailman/__init__.py
+++ byro_mailman/__init__.py
@@ -11,7 +11,7 @@ class PluginApp(AppConfig):
         author = 'rixx'
         description = ugettext_lazy('Mailing list integration for byro')
         visible = True
-        version = '0.0.0'
+        version = '1.0.1'
 
     def ready(self):
         from . import signals  # NOQA
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ cmdclass = {
 
 setup(
     name='byro-mailman',
-    version='1.0.0',
+    version='1.0.1',
     description='Mailing list integration for byro',
     long_description=long_description,
     url='https://github.com/byro/byro-mailman',
